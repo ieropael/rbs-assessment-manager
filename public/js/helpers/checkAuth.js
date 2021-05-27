1f60aec49eaa2f6c3a44e365dbe5fd747f2bf2ef
dev.v.dolgov <dev.v.dolgov@rainbowsoft.ru>
2021-05-27 18:01:39 +0400

    add checkAuth helper

---
+++ public/js/helpers/checkAuth.js
@@ -0,0 +1,7 @@
+import authModel from "../src/models/authModel.js";
+
+// функция проверяет авторизованность клиента
+// вызывает callback функцию с boolean аргументом - признаком авторизованности
+export function checkAuth(callback) {
+
+}
